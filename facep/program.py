f419dbe424d1e4a5630670450d6e314e38527b82
J3 <shs5481@naver.com>
2021-11-08 10:50:04 +0900

    ㅎㅎ

--- facep/program.py
+++ facep/program.py
@@ -15,7 +15,7 @@ try:
     cur = conn.cursor()
     cur2 = conn.cursor()
 except:
-    print("디비 연결 실패ㅎㅎ")
+    print("디비 연결 실패ㅎㅎffffff")
 
 ui = uic.loadUiType("face.ui")[0]
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
